6a520a0052324d771bb1af87992bca2a1acfc689
RLazamh <roger.laza@epn.edu.ec>
2022-08-30 23:06:59 -0500

    add module product

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-  console.log(`App run in PORT ${process.env.PORT}`)
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes( 
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }));
+  await app.listen(process.env.APP_PORT);
+  console.log(`Teslo Shop is running in port: ${process.env.APP_PORT}`)
 }
 bootstrap();
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService],
-  imports: [
-    TypeOrmModule.forFeature([ Product ])
-  ]
+  providers: [ProductService]
 })
 export class ProductModule {}
